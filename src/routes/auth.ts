19cc2f3451b1e94e37c7b5265128368e884d8b38
YamilCr <cruzyamil17@gmail.com>
2026-05-09 19:17:31 -0300

    registrar solo para auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express'
 import { authController } from '../controllers/authController'
-import { requireAuth } from '../middlewares/auth'
+import { requireAdmin, requireAuth } from '../middlewares/auth'
 
 const router = Router()
 
-router.post('/register', authController.register)
+router.post('/register', requireAuth,  authController.register)
 router.post('/login',    authController.login)
 router.get('/me',        requireAuth, authController.me)
 router.post('/logout',   requireAuth, authController.logout)
